53ae0f87630345e6f586f701ec7d40f97645c1b6
Tilinina Alexandra <Sasha_til@mail.ru>
2016-11-19 17:55:17 +0300

    weather parser

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.filter.ElementFilter;
+import org.jdom2.input.SAXBuilder;
+
+import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SAXBuilder parser = new SAXBuilder();
+        Document xmlDoc;
+
+        /* берем и коннектимся к апи
+        парсим xml с данными
+        api.openweathermap.org/data/2.5/weather?id=551487&mode=xml
+        xmlDoc.getRootElement().getContent(new ElementFilter("city"));
+        */
+        //Получаем XML с погодой.
+        try {
+            xmlDoc = parser.build(new URL("http://api.openweathermap.org/data/2.5/forecast?q=Kazan&mode=xml&appid=c897b438605eff38e5df5f6928351367&units=metric"));
+            List elements = xmlDoc.getRootElement().getContent(new ElementFilter("forecast"));
+            Iterator iterator = elements.iterator();
+            while(iterator.hasNext()) {
+                Element forecast = (Element)iterator.next();
+                List mixedCo = forecast.getChildren("time");
+                Iterator itr = mixedCo.iterator();
+                while (itr.hasNext()) {
+                    Element day = (Element)itr.next();
+                    //Узнаем дату
+                    String StrDate = day.getChildText("day");
+                    //Переведем их в более удобный формат
+                    String dt2 = "";
+                   /* for (int i = StrDate.length() - 1; i >= 0; i--) {
+                        if (StrDate.charAt(i) != '-')
+                            dt2 += StrDate.charAt(i);
+                    }*/
+                    System.out.println("Прогноз на: " + StrDate);
+                    //Температура
+                    Element temperature = day.getChild("temperature");
+                    String max_temp = temperature.getChildText("max");
+                    String min_temp = temperature.getChildText("min");
+                    System.out.println("Температура \nМаксимум: " + max_temp + "\nМинимум: " + min_temp);
+                }
+            }
+        }
+        catch(JDOMException | NullPointerException | java.io.IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
